8f198041932cd0f5c661fac01113ff2bd73989f5
Fizryan <52873306+Fizryan@users.noreply.github.com>
2025-03-17 01:10:44 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Fizryan");
+
+        DataGeneric<int> nimData = new DataGeneric<int>(103022300158);
+        nimData.PrintData();
+    }
+}
